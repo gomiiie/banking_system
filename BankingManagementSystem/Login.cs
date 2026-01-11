bbe4678029c79d1caad5f10832ae955e642ccd41
sumaiya <sumaiya0khan4@gmail.com>
2026-01-11 14:37:48 +0600

    asd123

--- BankingManagementSystem/Login.cs
+++ BankingManagementSystem/Login.cs
@@ -16,5 +16,10 @@ namespace BankingManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
