089b6722939c2e0803f0f859a74b742da959f7b9
sumaiya <sumaiya0khan4@gmail.com>
2026-01-15 23:40:05 +0600

    registration banker

---
+++ BankingManagementSystem/RegistrationBanker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankingManagementSystem
+{
+    public partial class RegistrationBanker : Form
+    {
+        public RegistrationBanker()
+        {
+            InitializeComponent();
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
