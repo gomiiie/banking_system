5ae93551f12037c86c6d97279f9741e93746376b
sumaiya <sumaiya0khan4@gmail.com>
2026-01-17 01:05:48 +0600

    Banker Registration

--- BankingManagementSystem/RegistrationBanker.Designer.cs
+++ BankingManagementSystem/RegistrationBanker.Designer.cs
@@ -28,35 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblName = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.namee = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Passwoo = new System.Windows.Forms.Label();
+            this.brnn = new System.Windows.Forms.Label();
+            this.tinn = new System.Windows.Forms.Label();
+            this.dobb = new System.Windows.Forms.Label();
+            this.numberr = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.adree = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // lblName
+            // namee
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(148, 149);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(71, 25);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "hgjfdk";
-            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            this.namee.AutoSize = true;
+            this.namee.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namee.Location = new System.Drawing.Point(138, 84);
+            this.namee.Name = "namee";
+            this.namee.Size = new System.Drawing.Size(65, 25);
+            this.namee.TabIndex = 2;
+            this.namee.Text = "Name";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(271, 152);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // Passwoo
+            // 
+            this.Passwoo.AutoSize = true;
+            this.Passwoo.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Passwoo.Location = new System.Drawing.Point(138, 119);
+            this.Passwoo.Name = "Passwoo";
+            this.Passwoo.Size = new System.Drawing.Size(99, 25);
+            this.Passwoo.TabIndex = 4;
+            this.Passwoo.Text = "Password";
+            // 
+            // brnn
+            // 
+            this.brnn.AutoSize = true;
+            this.brnn.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnn.Location = new System.Drawing.Point(139, 152);
+            this.brnn.Name = "brnn";
+            this.brnn.Size = new System.Drawing.Size(52, 25);
+            this.brnn.TabIndex = 6;
+            this.brnn.Text = "BRN";
+            // 
+            // tinn
+            // 
+            this.tinn.AutoSize = true;
+            this.tinn.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tinn.Location = new System.Drawing.Point(138, 193);
+            this.tinn.Name = "tinn";
+            this.tinn.Size = new System.Drawing.Size(44, 25);
+            this.tinn.TabIndex = 7;
+            this.tinn.Text = "TIN";
+            // 
+            // dobb
+            // 
+            this.dobb.AutoSize = true;
+            this.dobb.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dobb.Location = new System.Drawing.Point(138, 236);
+            this.dobb.Name = "dobb";
+            this.dobb.Size = new System.Drawing.Size(53, 25);
+            this.dobb.TabIndex = 8;
+            this.dobb.Text = "DOB";
+            // 
+            // numberr
+            // 
+            this.numberr.AutoSize = true;
+            this.numberr.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberr.Location = new System.Drawing.Point(138, 272);
+            this.numberr.Name = "numberr";
+            this.numberr.Size = new System.Drawing.Size(87, 25);
+            this.numberr.TabIndex = 9;
+            this.numberr.Text = "Number";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(320, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 24);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(320, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 24);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(320, 197);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(183, 24);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(320, 236);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(183, 24);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(320, 162);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(183, 24);
+            this.textBox9.TabIndex = 17;
+            // 
+            // adree
+            // 
+            this.adree.AutoSize = true;
+            this.adree.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adree.Location = new System.Drawing.Point(139, 317);
+            this.adree.Name = "adree";
+            this.adree.Size = new System.Drawing.Size(86, 25);
+            this.adree.TabIndex = 18;
+            this.adree.Text = "Address";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(320, 272);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 27);
+            this.dateTimePicker1.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 37);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Company Registration";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnReg
+            // 
+            this.btnReg.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReg.Location = new System.Drawing.Point(380, 387);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(87, 29);
+            this.btnReg.TabIndex = 22;
+            this.btnReg.Text = "register";
+            this.btnReg.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 317);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(200, 47);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
             // 
             // RegistrationBanker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.lblName);
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnReg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.adree);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.numberr);
+            this.Controls.Add(this.dobb);
+            this.Controls.Add(this.tinn);
+            this.Controls.Add(this.brnn);
+            this.Controls.Add(this.Passwoo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.namee);
+            this.Font = new System.Drawing.Font("Symbol", 8.25F);
             this.Name = "RegistrationBanker";
             this.Text = "RegistrationBanker";
             this.ResumeLayout(false);
@@ -65,8 +233,26 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblName;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.Label namee;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Passwoo;
+        private System.Windows.Forms.Label brnn;
+        private System.Windows.Forms.Label tinn;
+        private System.Windows.Forms.Label dobb;
+        private System.Windows.Forms.Label numberr;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label adree;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReg;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- BankingManagementSystem/RegistrationBanker.cs
+++ BankingManagementSystem/RegistrationBanker.cs
@@ -21,5 +21,20 @@ namespace BankingManagementSystem
         {
 
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
